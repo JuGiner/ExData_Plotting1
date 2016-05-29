3bc68eb4f9c400ec5b315c8811e35e6d5f306179
JuGiner <j.giner.delgado@gmail.com>
2016-05-29 19:29:47 +0200

    Project 1 of Exploratory Data Analysis Coursera Course

---
+++ plot1.R
@@ -0,0 +1,31 @@
+# Download and read data
+#--------------------------------------------------------------
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl, destfile = "household_power_consumption.zip", method = "curl")
+unzip("household_power_consumption.zip")
+epc <- read.table("./household_power_consumption.txt", sep = ";", 
+                  header = TRUE, stringsAsFactors = FALSE, na.strings = "?")
+
+# Defining variables
+#--------------------------------------------------------------
+height <- 480
+width <- 480
+
+# Extracting util data
+#--------------------------------------------------------------
+# Convert Date/time
+epc$DateTime <- paste(epc$Date, epc$Time)
+epc$DateTime <- strptime(epc$DateTime, "%d/%m/%Y %H:%M:%S")
+epc$Date <- as.Date(strptime(epc$Date, "%d/%m/%Y"))
+t1 <- as.Date("2007-02-01")
+t2 <- as.Date("2007-02-02")
+# Subset
+dat <- subset(epc, Date == t1 | Date == t2)
+
+
+# Making the plot
+#--------------------------------------------------------------
+png("plot1.png", width = width, height = height)
+hist(dat$Global_active_power, col = "red", 
+     xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,46 @@
+# Download and read data
+#--------------------------------------------------------------
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl, destfile = "household_power_consumption.zip", method = "curl")
+unzip("household_power_consumption.zip")
+epc <- read.table("./household_power_consumption.txt", sep = ";", 
+                  header = TRUE, stringsAsFactors = FALSE, na.strings = "?")
+
+# Defining variables
+#--------------------------------------------------------------
+height <- 480
+width <- 480
+
+# Extracting util data
+#--------------------------------------------------------------
+# Convert Date/time
+epc$DateTime <- paste(epc$Date, epc$Time)
+epc$DateTime <- strptime(epc$DateTime, "%d/%m/%Y %H:%M:%S")
+epc$Date <- as.Date(strptime(epc$Date, "%d/%m/%Y"))
+t1 <- as.Date("2007-02-01")
+t2 <- as.Date("2007-02-02")
+# Subset
+dat <- subset(epc, Date == t1 | Date == t2)
+
+# Making the plot
+#--------------------------------------------------------------
+png("plot4.png", width = width, height = height)
+par(mfrow = c(2, 2), mar = c(4, 4, 4, 2), oma = c(1, 0, 0, 0))
+# top-left
+plot(dat$DateTime, dat$Global_active_power, ylab = "Global Active Power", 
+     xlab = "", type = "l")
+# top-right
+plot(dat$DateTime, dat$Voltage, ylab = "Voltage", 
+     xlab = "datetime", type = "l")
+# bottom-left
+plot(dat$DateTime, dat$Sub_metering_1, ylab = "Energy sub metering", 
+     xlab = "", type = "n")
+lines(dat$DateTime, dat$Sub_metering_1, col = "black")
+lines(dat$DateTime, dat$Sub_metering_2, col = "red")
+lines(dat$DateTime, dat$Sub_metering_3, col = "blue")
+legend("topright", bty = "n", lwd=1, col = c("black", "red", "blue"), 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+# bottom-right
+plot(dat$DateTime, dat$Global_reactive_power, ylab = "Global_reactive_power", 
+     xlab = "datetime", type = "l")
+dev.off()
